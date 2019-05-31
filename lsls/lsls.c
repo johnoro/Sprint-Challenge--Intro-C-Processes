7906cfe1ce7ddc25b9c11c5f54c8b4c8ecca941d
John O'Rourke <officialjohnorourke@gmail.com>
2019-05-31 13:32:57 -0400

    Expands the heirarchy with room for more nested dir reading

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -4,23 +4,12 @@
 #include <dirent.h>
 #include <sys/stat.h>
 
-/**
- * Main
- */
-int main(int argc, char **argv) {
-  char *dirname;
-
-  // Parse command line
-  if (argc > 2) {
-    fprintf(stderr, "\nUsage: ./lsls.exe [directory], i.e.: ./lsls.exe ../examples\n");
-    exit(1);
-  } else
-    dirname = argc == 2 ? argv[1] : ".";
-
+void print_dir(char *dirname, int width, int count) {
   // Open directory
   DIR *d = opendir(dirname);
 
   if (d == NULL) {
+    // printf("\n%s\n", dirname);
     fprintf(stderr, "\nRequires a valid directory!\nUsage: ./lsls.exe [directory], i.e.: ./lsls.exe ../examples\n");
     exit(1);
   }
@@ -38,14 +27,32 @@ int main(int argc, char **argv) {
     // printf("path: %s\n", path);
     stat(path, &buf);
     if (S_ISDIR(buf.st_mode) == 0) {
-      printf("%10ld %s\n", buf.st_size, ent->d_name);
+      printf("%*ld %s\n", width, buf.st_size, ent->d_name);
     } else {
-      printf("%10s %s\n", "<DIR>", ent->d_name);
+      printf("%*s %s\n", width, "<DIR>", ent->d_name);
+      if (count)
+        print_dir(path, width+10, count-1);
     }
   }
 
   // Close directory
   closedir(d);
+}
+
+/**
+ * Main
+ */
+int main(int argc, char **argv) {
+  char *dirname;
+
+  // Parse command line
+  if (argc > 2) {
+    fprintf(stderr, "\nUsage: ./lsls.exe [directory], i.e.: ./lsls.exe ../examples\n");
+    exit(1);
+  } else
+    dirname = argc == 2 ? argv[1] : ".";
+  
+  print_dir(dirname, 10, 1);
 
   return 0;
 }
